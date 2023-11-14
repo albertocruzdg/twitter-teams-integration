cff6b4b58ab85a162735c451fddc9e68b6ba8af2
Alberto Cruz <cruz.alberto@gmail.com>
2023-11-14 15:10:34 -0300

    initial commit

---
+++ TwitterTeamsIntegration.API/MicrosoftGraphOptions.cs
@@ -0,0 +1,12 @@
+namespace TwitterTeamsIntegration.API;
+
+internal sealed class MicrosoftGraphOptions
+{
+    public string ClientId { get; set; }
+    
+    public string TenantId { get; set; }
+    
+    public string ClientSecret { get; set; }
+    
+    public string NotificationUrl { get; set; }
+}
---
+++ TwitterTeamsIntegration.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.Cryptography.X509Certificates;
+
+var certificate = new X509Certificate2();
+
+//certificate.Import(Convert.FromBase64String());
